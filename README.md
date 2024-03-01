6a536e643bbf44ff2c2811f242fdf37a4654ebc5
Antonio Alonzi <aalonzi@ebay.com>
2024-03-01 15:10:59 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # chess-ai
+
+A chess game with ai copied from https://www.youtube.com/watch?v=OpL0Gcfn4B4
+
+
+# Build
+
+    pip3 install pygame
---
+++ src/const.py
@@ -0,0 +1,12 @@
+# Screen dimensions
+WIDTH = 800
+HEIGHT = 800
+
+# Board dimensions
+COLS = 8
+ROWS = 8
+SQSIZE = WIDTH // COLS
+
+# Board colors
+LIGHT_GREEN = (234, 235, 200)
+DARK_GREY = (119, 154, 88)
